3db5b264eba582fc4593db4da1d2980accbb871a
Arnaudb78 <arnaudb92230@gmail.com>
2023-10-12 12:03:41 +0200

    V 1.0.0 :fire:

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ from PySide6.QtCore import *
 from PySide6.QtWidgets import *
 from PySide6.QtGui import *
 
+import random
+import string
+
 from pathlib import Path 
+
+import dialogs
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -44,18 +49,70 @@ class MainWindow(QMainWindow):
         self.option_size.setValue(10)
         option_layout.addWidget(self.option_size, 1, 0)
 
+        #params
         self.option_lowercase = QCheckBox("Minuscules")
         option_layout.addWidget(self.option_lowercase, 0,1)
         self.option_uppercase = QCheckBox("Majuscules")
         option_layout.addWidget(self.option_uppercase, 1, 1)
-        self.option_number = QCheckBox("Chiffres")
-        option_layout.addWidget(self.option_number, 0, 2)
+        self.option_numbers = QCheckBox("Chiffres")
+        option_layout.addWidget(self.option_numbers, 0, 2)
         self.option_symbols = QCheckBox("Symboles")
         option_layout.addWidget(self.option_symbols, 1, 2)
 
-        
+        #checkbox check open application
+        self.option_lowercase.setChecked(True)
+        self.option_numbers.setChecked(True)
 
-        
+        #display pass at open
+        self.generate()
+
+        #Connect btn
+        btn_quit.clicked.connect(self.quit)
+        btn_copy.clicked.connect(self.copy)
+
+        self.option_size.valueChanged.connect(self.change_size)
+
+        btn_generate.clicked.connect(self.generate)
+
+    #Action btn
+    def quit(self):
+        if dialogs.confirm(self):
+            QApplication.quit() 
+
+    def copy(self):
+        clipboard = QApplication.clipboard()
+        clipboard.setText(self.password_generated.text())
+
+        #recupère la valeur => affiche
+    def change_size(self):
+        value = self.option_size.value()
+        self.txt_size.setText("Taille : " + str(value))
+
+        #add different options => generate password!
+    def generate(self):
+        size = self.option_size.value()
+        has_lower = self.option_lowercase.isChecked()
+        has_upper = self.option_uppercase.isChecked()
+        has_numbers = self.option_numbers.isChecked()
+        has_symbols = self.option_symbols.isChecked() 
+
+        mdp = "azertyuiopqsdfghjklmwxcvbn"
+
+        if has_upper: 
+            mdp += "AZERTYUIOPQSDFGHJKLMWXCVBN"
+        if has_numbers: 
+            mdp += "0987654321"
+        if has_symbols:
+            mdp += "!@#$%^&*()_-+=<>?/"
+
+        # mdp = ''.join(random.choice(mdp) for _ in range(size))
+        # self.password_generated.setText(mdp)
+
+        ## Correction
+        password = ""
+        for i in range(size):
+            password += random.choice(mdp)
+        self.password_generated.setText(password)
 
 app = QApplication(sys.argv)
 
